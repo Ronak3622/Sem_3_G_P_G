c1c49714cf709716cede576157effeb2da29eea1
RONAKKUMAR MANUBHAI RATHOD <128444759+Ronak3622@users.noreply.github.com>
2023-12-19 00:49:55 +0530

    Create table.sql

---
+++ RDBMS/table.sql
@@ -0,0 +1,13 @@
+-- Table 
+CREATE TABLE student
+(id number(5),
+    name varchar2(15),
+    city varchar2(15)
+    );
+
+insert into student values(111,'ramesh','surat');
+insert into student values(112,'mahesh','surat');
+insert into student values(113,'rajiv','gandhinagar');
+insert into student values(114, 'raj','gandhinagar');
+insert into student values(115,'vijay','baroda');
+insert into student values(116,'jay','baroda');
