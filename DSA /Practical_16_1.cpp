07c2fd9034bccc7331f3f52a9aa108c6bdde2297
RONAKKUMAR MANUBHAI RATHOD <128444759+Ronak3622@users.noreply.github.com>
2023-12-25 10:55:20 +0530

    Create Practical_16_1.cpp

---
+++ DSA /Practical_16_1.cpp
@@ -0,0 +1,47 @@
+// Practical No. 16_1  Implement algorithm to delete a node from beginning in singly linked list.
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node *next;
+    node(int d){
+        data = d;
+        node *next = NULL;
+    }
+};
+void insertAtFirstNode(node *&head, int data){
+    node *n = new node(data);
+    n->next = head;
+    head = n;
+}
+void print(node *head){
+    while (head != NULL){
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << endl;
+}
+void deleteAtFirst(node *&head){
+    if (head == NULL){
+        return;
+    }
+    node *temp = head;
+    head = head->next;
+    delete temp;
+    return;
+}
+int main(){
+    node *head = NULL;
+    insertAtFirstNode(head, 1);
+    insertAtFirstNode(head, 2);
+    insertAtFirstNode(head, 3);
+    insertAtFirstNode(head, 4);
+    deleteAtFirst(head);
+    print(head);
+}
+/*
+Output :-
+3->2->1->
+*/
