62138e9df4debee5857901fa11b29de760f793e1
RONAKKUMAR MANUBHAI RATHOD <128444759+Ronak3622@users.noreply.github.com>
2024-02-11 13:37:31 +0530

    Create AA_Practical_01.java

---
+++ Sem_4th/AOOP/AA_Practical_01.java
@@ -0,0 +1,11 @@
+// Practical : 1 : Install JDK, Write a simple "Hello World!" or similar java program, compilation, debugging, executing using java compiler and interpreter.
+public class AA_Practical_01 {
+    public static void main(String []args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+/*
+Output:
+Hello World!
+*/
