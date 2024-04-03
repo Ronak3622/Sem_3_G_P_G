71422e375a564bf0586300e2d0063b7f37df52db
RONAKKUMAR MANUBHAI RATHOD <128444759+Ronak3622@users.noreply.github.com>
2024-04-04 04:46:46 +0530

    Create mid.php

---
+++ PHP/mid.php
@@ -0,0 +1,71 @@
+<?php
+//Single Line Comment
+/*
+This 
+is
+multi Line Comment
+*/
+
+// 1. echo And print
+//echo "Hello World!";
+
+//2. Create variable
+/*
+$a = 10;
+$b = 20;
+echo $a + $b;
+*/
+
+//3. Arithmetic Operators
+/*
+$a = 10;
+$b = 20;
+
+echo $a + $b. "<br>";
+echo $a - $b. "<br>";
+echo $a * $b. "<br>";
+echo $a / $b. "<br>";
+echo $a % $b. "<br>";
+echo $a ** $b. "<br>";
+*/
+
+//4. Increment - Decrement Operators
+/*
+$a = 10;
+$b = 10;
+$c = 10;
+$d = 10;
+
+echo ++$a."<br>";
+echo $b++."<br>";
+echo --$c."<br>";
+echo $d--."<br>";
+*/
+
+//5. Assignment Operators
+/*
+$a = 10;
+$b = 20;
+
+//echo $a = $b."<br>";
+//echo $a += $b."<br>";
+/echo $a -= $b."<br>";
+echo $a *= $b."<br>";
+echo $a /= $b."<br>";
+echo $a %= $b."<br>";
+*/
+
+//6. Logical Operators
+
+//7. Bitwise Operators
+
+//8. Comparison Operators
+
+//9. If Statement 
+
+$a = 19;
+if($a>18){
+    echo "You are 18+.";
+}
+echo "Thanks You!";
+?>
